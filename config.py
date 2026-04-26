f9e650befba9bcca0e4046e7053a1994d25378ef
abreuzzzz <105026818+abreuzzzz@users.noreply.github.com>
2026-04-26 07:54:05 -0300

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,7 @@ GOOGLE_CREDS_JSON             = os.environ["GOOGLE_CREDS_JSON"]
 
 CLIENT_ID                     = os.environ["CONTA_AZUL_CLIENT_ID"]
 CLIENT_SECRET                 = os.environ["CONTA_AZUL_CLIENT_SECRET"]
-CONTA_AZUL_BASE_URL           = "https://api-v2.contaazul.com/financeiro/v1"
 CONTA_FINANCEIRA_RECEBER_ID   = os.environ.get("CONTA_FINANCEIRA_RECEBER_ID", "")
 CONTA_FINANCEIRA_PAGAR_ID     = os.environ.get("CONTA_FINANCEIRA_PAGAR_ID", "")
-CATEGORIA_RECEBER_ID          = os.environ.get("CATEGORIA_RECEBER_ID", "")
-CATEGORIA_PAGAR_ID            = os.environ.get("CATEGORIA_PAGAR_ID", "")
+CATEGORIA_RECEBER_ID          = os.environ["CATEGORIA_RECEBER_ID"]
+CATEGORIA_PAGAR_ID            = os.environ["CATEGORIA_PAGAR_ID"]
