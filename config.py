c7cae2a7a64a9aeacbac88505fcb0a256f3f9f75
abreuzzzz <105026818+abreuzzzz@users.noreply.github.com>
2026-04-28 09:50:27 -0300

    Update config.py

--- config.py
+++ config.py
@@ -10,3 +10,5 @@ CONTA_FINANCEIRA_RECEBER_ID   = os.environ.get("CONTA_FINANCEIRA_RECEBER_ID", ""
 CONTA_FINANCEIRA_PAGAR_ID     = os.environ.get("CONTA_FINANCEIRA_PAGAR_ID", "")
 CATEGORIA_RECEBER_ID          = os.environ["CATEGORIA_RECEBER_ID"]
 CATEGORIA_PAGAR_ID            = os.environ["CATEGORIA_PAGAR_ID"]
+CENTRO_CUSTO_RECEBER_ID       = os.environ.get("CENTRO_CUSTO_RECEBER_ID", "")  # novo
+CENTRO_CUSTO_PAGAR_ID         = os.environ.get("CENTRO_CUSTO_PAGAR_ID", "")    # novo
